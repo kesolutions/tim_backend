40c1fed5ecc05b85c325143bf6f6e1123befb2a3
TimTan <timtan2024@gmail.com>
2024-01-19 16:46:41 -0500

    final

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,5 @@
-const client = require('./connection'); 
-
-
+const { MongoClient } = require('mongodb')
+const client = new MongoClient('mongodb://localhost:27017')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/user', userRoutes);
+
+app.use('/api', require('./routes/company'));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
@@ -16,3 +19,5 @@ app.listen(port, () => {
 client.connect()
     .then(() => console.log('Connected Successfully'))
     .catch(error => console.log('Failed to connect', error))
+
+module.exports = app;
